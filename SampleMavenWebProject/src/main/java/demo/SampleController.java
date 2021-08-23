ef57eb42d926a8da3af674fe8fb5a0bd8ee627e8
Leelavathy <durga263586@gmail.com>
2021-08-23 11:22:34 +0530

    Sample Maven Project

---
+++ SampleMavenWebProject/src/main/java/demo/SampleController.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class SampleController {
+	
+	@RequestMapping(value = "/sample", method = RequestMethod.GET)
+	public String testMavenMVC() {
+		System.out.println("Welcome to Maven MVC");
+		return "test";
+	}
+}
